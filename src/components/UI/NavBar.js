0c7c2f286a40396bede907383091889ac5ecfbab
Nathalie Liu <liunathalie@hotmail.fr>
2023-04-25 15:19:22 +0200

    MAJ NavBar

---
+++ src/components/UI/NavBar.js
@@ -0,0 +1,24 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo">
+          FRIGOAL
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="">Home</a>
+          </li>
+          <li>
+            <a href="">Fridges</a>
+          </li>
+          <li>
+            <a href="">Recipes</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
