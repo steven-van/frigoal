e164d6c135d0f55d4f3dd70afe5ffad34a115186
MohamadReslan <115694912+MohamadReslan@users.noreply.github.com>
2023-04-29 18:57:07 +0200

    add footer

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,4 +1,5 @@
 import Logo from "../assets/imgs/png/OK.jpg";
+import Footer from "../components/UI/Footer";
 
 const Profile = ({ firstName, lastName, email, password }) => {
   const inputItems = [
@@ -9,35 +10,38 @@ const Profile = ({ firstName, lastName, email, password }) => {
   ];
 
   return (
-    <div className="grid place-items-center pt-8">
-      <div className="text-center">
-        <img className="h-48 w-48 rounded-full" src={Logo} />
-        <p className="text-xl font-bold">Mohamad RESLAN</p>
-      </div>
-      <div className="mt-16">
-        <div className="">
-          <p className="text-xl font-bold">Profile</p>
-          <p className="text-xs ">
-            This information will be displayed publicly so be careful what you
-            share.
-          </p>
+    <div>
+      <div className="grid place-items-center pt-8 h-screen">
+        <div className="text-center ">
+          <img className="h-48 w-48 rounded-full" src={Logo} />
+          <p className="text-xl font-bold">Mohamad RESLAN</p>
         </div>
-        <div className="grid grid-cols-2">
-          {inputItems.map((item) => (
-            <div className="mt-4">
-              <div>{item.label}</div>
-              <input
-                className="border-2 border-black-900"
-                type="text"
-                value={item.value}
-                id="name"
-                name="name"
-                required
-              />
-            </div>
-          ))}
+        <div className="mt-16">
+          <div className="">
+            <p className="text-xl font-bold">Profile</p>
+            <p className="text-xs ">
+              This information will be displayed publicly so be careful what you
+              share.
+            </p>
+          </div>
+          <div className="grid grid-cols-2">
+            {inputItems.map((item) => (
+              <div className="mt-4">
+                <div>{item.label}</div>
+                <input
+                  className="border-2 border-black-900"
+                  type="text"
+                  value={item.value}
+                  id="name"
+                  name="name"
+                  required
+                />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
