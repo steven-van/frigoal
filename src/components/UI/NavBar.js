5dbf077aa3a97e37fbc9a396f91cd88cd2b40788
Nathalie Liu <liunathalie@hotmail.fr>
2023-05-01 23:11:28 +0200

    Ajout dropdown account + MAJ logo / couleur

--- src/components/UI/NavBar.js
+++ src/components/UI/NavBar.js
@@ -1,5 +1,5 @@
-import logo from "../../assets/imgs/png/logo-with-text.png";
-import defaultAvatar from "../../assets/imgs/svg/avatar.svg";
+import logo from "../../assets/imgs/svg/logo-with-text.svg";
+import Dropdown from "./Dropdown";
 
 const NavBar = () => {
   return (
@@ -13,34 +13,25 @@ const NavBar = () => {
         <div class="flex justify-center gap-4 text-sm lg:flex-grow">
           <a
             href="#home"
-            class="inline-block text-lg px-5 py-2 leading-none border-2 rounded-full text-black border-[#0BA95D] hover:border-transparent hover:text-white hover:bg-[#0BA95D] mt-4 lg:mt-0"
+            class="inline-block text-lg px-5 py-2 leading-none rounded-full text-black hover:border-transparent hover:text-white hover:bg-[#65C9FF] mt-4 lg:mt-0"
           >
             Home
           </a>
           <a
             href="#fridges"
-            class="inline-block text-lg px-5 py-2 leading-none border-2 rounded-full text-black border-[#0BA95D] hover:border-transparent hover:text-white hover:bg-[#0BA95D] mt-4 lg:mt-0"
+            class="inline-block text-lg px-5 py-2 leading-none rounded-full text-black hover:border-transparent hover:text-white hover:bg-[#65C9FF] mt-4 lg:mt-0"
           >
             Fridges
           </a>
           <a
-            href=""
-            class="inline-block text-lg px-5 py-2 leading-none border-2 rounded-full text-black border-[#0BA95D] hover:border-transparent hover:text-white hover:bg-[#0BA95D] mt-4 lg:mt-0"
+            href="#recipes"
+            class="inline-block text-lg px-5 py-2 leading-none rounded-full text-black hover:border-transparent hover:text-white hover:bg-[#65C9FF] mt-4 lg:mt-0"
           >
             Recipes
           </a>
         </div>
         <div className="absolute right-0">
-          <a
-            href="#responsive-header"
-            class="overflow-hidden h-16 w-16 inline-block text-lg leading-none border-2 border-[#FEF5DA] rounded-full text-black hover:border-[#0BA95D] hover:text-white mt-4 lg:mt-0"
-          >
-            <img
-              class="object-center h-20 w-20"
-              src={defaultAvatar}
-              alt="Avatar"
-            ></img>
-          </a>
+        <Dropdown/>
         </div>
       </div>
     </nav>
