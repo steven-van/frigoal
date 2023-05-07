41419416e46c55efdc70ad3aad18971904e35481
Nathalie Liu <liunathalie@hotmail.fr>
2023-05-07 15:35:04 +0200

    Correction des boutons "SignUp/Login"

--- src/components/UI/Dropdown.js
+++ src/components/UI/Dropdown.js
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { Menu, Transition } from "@headlessui/react";
+import { useNavigate } from "react-router-dom";
 
 import defaultAvatar from "../../assets/imgs/svg/avatar.svg";
 import logoLogin from "../../assets/imgs/svg/logo-login.svg";
@@ -11,70 +12,9 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-const notLoggedIn = () => {
-  return (
-    <Menu as="div" className="relative inline-block text-left">
-      <div>
-        <Menu.Button className="inline-flex bg-[#6097CE] overflow-hidden h-14 w-14 justify-center gap-x-1.5 rounded-full shadow-sm hover:ring-1 hover:ring--[#65C9FF]">
-          <img
-            class="object-center w-full"
-            src={defaultAvatar}
-            alt="Avatar"
-          ></img>
-        </Menu.Button>
-      </div>
+const Dropdown = () => {
+  const navigate = useNavigate();
 
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="py-1">
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#LogIn"
-                  className={classNames(
-                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                    "px-4 py-2 w-full text-sm flex justify-start items-center"
-                  )}
-                >
-                  <img class="w-10 mr-3" src={logoLogin} alt="logo login"></img>
-                  Log in
-                </a>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <a
-                  href="#SignUp"
-                  className={classNames(
-                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                    "px-4 py-2 w-full text-sm flex justify-start items-center"
-                  )}
-                >
-                  <img
-                    class="w-10 mr-3"
-                    src={logoSignUp}
-                    alt="logo sign up"
-                  ></img>
-                  Sign up
-                </a>
-              )}
-            </Menu.Item>
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
-  );
-};
-
-const loggedIn = () => {
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
@@ -104,7 +44,7 @@ const loggedIn = () => {
             <Menu.Item>
               {({ active }) => (
                 <a
-                  href="#AccountSettings"
+                  href="/Profile"
                   className={classNames(
                     active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                     "px-4 py-2 w-full text-sm flex justify-start items-center"
@@ -144,11 +84,4 @@ const loggedIn = () => {
   );
 };
 
-export default function Dropdown() {
-  var login = false;
-  if (login === true) {
-    return loggedIn();
-  } else {
-    return notLoggedIn();
-  }
-}
+export default Dropdown;
--- src/components/UI/NavBar.js
+++ src/components/UI/NavBar.js
@@ -1,51 +1,63 @@
 import logo from "../../assets/imgs/svg/logo-with-text.svg";
-import buttonFridge from "../../assets/imgs/svg/button-fridge.svg";
-
+import { useNavigate } from "react-router-dom";
 import Dropdown from "./Dropdown";
 
+const Button = (url, title) => {
+  return (
+    <a
+      href={url}
+      class="inline-block text-lg px-5 py-2 leading-none rounded-full text-black hover:border-transparent hover:text-white hover:bg-[#65C9FF] active:border-transparent active:text-white active:bg-[#65C9FF] mt-4 lg:mt-0"
+    >
+      {title}
+    </a>
+  );
+};
+
 const NavBar = () => {
-  var login = true;
+  var isLoggedIn = true;
+
+  const navigate = useNavigate();
+
+  const LogoOnClick = () => {
+    navigate("/");
+  };
+
   return (
     <nav class="flex items-center justify-between flex-wrap bg-[#FFFFFF] p-10">
       <div className="w-full flex relative justify-center items-center">
         <div className="absolute left-0">
-          <div class="w-44">
+          <div class="w-44" onClick={() => LogoOnClick()}>
             <img src={logo} alt="Frigoal Logo"></img>
           </div>
         </div>
         <div class="flex justify-center gap-4 text-sm lg:flex-grow">
-          <a
-            href="#home"
-            class="inline-block text-lg px-5 py-2 leading-none rounded-full text-black hover:border-transparent hover:text-white hover:bg-[#65C9FF] active:border-transparent active:text-white active:bg-[#65C9FF] mt-4 lg:mt-0"
-          >
-            Home
-          </a>
-          <a
-            href="#fridges"
-            class="inline-block text-lg px-5 py-2 leading-none rounded-full text-black hover:border-transparent hover:text-white hover:bg-[#65C9FF] active:border-transparent active:text-white active:bg-[#65C9FF] mt-4 lg:mt-0"
-          >
-            Fridges
-          </a>
-          <a
-            href="#recipes"
-            class="inline-block text-lg px-5 py-2 leading-none rounded-full text-black hover:border-transparent hover:text-white hover:bg-[#65C9FF] active:border-transparent active:text-white active:bg-[#65C9FF] mt-4 lg:mt-0"
-          >
-            Recipes
-          </a>
+          {Button("/", "Home")}
+          {Button("#Fridges", "Fridges")}
+          {Button("#Recipes", "Recipes")}
         </div>
         <div className="flex flex-row justify-center items-center absolute right-0">
-          {login === true && (
-            <div className="h-14 w-14 bg-[#E1F2FF] mr-8 mb-1 flex justify-center items-center rounded-lg ">
-              <img
-                src={buttonFridge}
-                alt="Fridge Logo"
-                className="h-10 w-10"
-              ></img>
+          {isLoggedIn ? (
+            <div className="mr-8 mb-1 flex justify-center items-center">
+              <Dropdown />
+            </div>
+          ) : (
+            <div className="mr-8 mb-1 flex justify-center items-center rounded-lg ">
+              <div class="flex justify-center gap-4 text-sm lg:flex-grow">
+                <a
+                  href="/Login"
+                  class="inline-block text-lg px-5 py-2 leading-none rounded-full text-white bg-[#6097CE] hover:bg-[#6097CE]/[.75] mt-4 lg:mt-0"
+                >
+                  Log In
+                </a>
+                <a
+                  href="/SignUp"
+                  class="inline-block text-lg px-5 py-2 leading-none rounded-full text-white bg-[#65C9FF] hover:bg-[#65C9FF]/[.75] mt-4 lg:mt-0"
+                >
+                  Sign Up
+                </a>
+              </div>
             </div>
           )}
-          <div className="">
-            <Dropdown />
-          </div>
         </div>
       </div>
     </nav>
