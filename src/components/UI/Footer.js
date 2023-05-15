f3e1af096ff99c602303543b016bd67de45bc832
MohamadReslan <115694912+MohamadReslan@users.noreply.github.com>
2023-05-15 16:00:02 +0200

    fix color

--- src/components/UI/Footer.js
+++ src/components/UI/Footer.js
@@ -17,22 +17,23 @@ const Footer = () => {
   ];
 
   return (
-
-    <div className="w-full bg-[#6097CE] left-0 bottom-0 p-2 fixed">
-      <div className="flex justify-center pb-2 ">
-        {footerItems.map((el) => (
-          <div className="w-16">
-            <a href={el.link}>{el.label}</a>
-          </div>
-        ))}
+    <footer>
+      <div className="w-full bg-[#6097CE] left-0 bottom-0 relative text-white">
+        <div className="flex justify-center pb-2 w-screen gap-6 pt-4">
+          {footerItems.map((el) => (
+            <div className="w-16">
+              <a href={el.link}>{el.label}</a>
+            </div>
+          ))}
+        </div>
+        <div className="flex justify-center">
+          <img className="w-16" src={GitHub} alt="GitHub" />
+        </div>
+        <div className="flex justify-center pt-2 ">
+          <p>2023 LSI2 - APP</p>
+        </div>
       </div>
-      <div className="flex justify-center">
-        <img className="w-16" src={GitHub} alt="GitHub" />
-      </div>
-      <div className="flex justify-center pt-2">
-        <p>2023 LSI2 - APP</p>
-      </div>
-    </div>
+    </footer>
   );
 };
 
