846270b6eb0f5cc418ec88d6c2daa7e55f2bf311
Nathalie Liu <liunathalie@hotmail.fr>
2023-05-15 15:02:56 +0200

    correction

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,7 @@ import NavBar from "../components/UI/NavBar";
 const Home = () => {
   return (
     <div>
-      <div>
-        <NavBar />
-      </div>
-      <div>hello</div>
+      <NavBar />
     </div>
   );
 };
