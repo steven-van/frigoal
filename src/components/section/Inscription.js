3da104b6cc3ed46cd854465c907e241d14d5e886
“fandresena” <christineli777@gmail.com>
2023-05-15 10:03:38 +0200

    add input for password confirmation

--- src/components/section/Inscription.js
+++ src/components/section/Inscription.js
@@ -74,9 +74,26 @@ const Inscription = () =>{
                               />
                             </div>
                           </div>
+
+                          <div>
+                            <div className="flex items-center justify-between">
+                              <label htmlFor="password" className={`block text-sm font-medium leading-6 text-gray-900 ${font}`}>
+                                Confirm your password
+                              </label>
+                            </div>
+                            <div className="mt-2">
+                              <input
+                                id="confirmPassword"
+                                name="confirmPassword"
+                                type="password"
+                                required
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                              />
+                            </div>
+                          </div>
       
                           <div>
-                          <br></br><br></br><br></br>
+                          <br></br><br></br>
                             <button
                               type="submit"
                               className={`flex w-full justify-center rounded-md bg-[#65C9FF] px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-[#6097CE] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ${font}`}
