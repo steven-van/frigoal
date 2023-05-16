cb7f60f2271d08132c91a7a2a7c4dd9c761e653d
Nathalie Liu <liunathalie@hotmail.fr>
2023-05-16 17:08:36 +0200

    update

--- src/components/UI/Fridge-card.js
+++ src/components/UI/Fridge-card.js
@@ -6,11 +6,13 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-const Fridge = ({ id, name }) => {
+const Fridge = ({ id, name, color }) => {
   return (
-    <div class="w-full max-w-sm m-1 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-      <Menu as="div" className="flex justify-end px-4 pt-4">
-        <Menu.Button className="inline-block text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-1.5">
+    <div
+      class={`h-64 w-80 max-w-sm m-2 p-4 overflow-hidden border border-gray-200 rounded-2xl relative shadow-md ${color}`}
+    >
+      <Menu as="div" className="flex absolute z-20 top-0 right-0 p-4">
+        <Menu.Button className="inline-block relative text-gray-500 hover:bg-blue-50 focus:ring-slate-50 focus:ring-4 focus:outline-none rounded-lg text-sm p-1.5">
           <svg
             class="w-6 h-6"
             aria-hidden="true"
@@ -20,77 +22,71 @@ const Fridge = ({ id, name }) => {
           >
             <path d="M6 10a2 2 0 11-4 0 2 2 0 014 0zM12 10a2 2 0 11-4 0 2 2 0 014 0zM16 12a2 2 0 100-4 2 2 0 000 4z"></path>
           </svg>
-        </Menu.Button>
 
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          {/* Dropdown menu */}
-          <Menu.Items className="z-10 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <div className="py-1">
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "px-4 py-2 w-full text-sm flex justify-start items-center"
-                    )}
-                  >
-                    Edit
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <a
-                    href="#"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "px-4 py-2 w-full text-sm flex justify-start items-center"
-                    )}
-                  >
-                    Export Data
-                  </a>
-                )}
-              </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type="submit"
-                    className={classNames(
-                      active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      "px-4 py-2 w-full text-sm flex justify-start items-center"
-                    )}
-                  >
-                    Delete
-                  </button>
-                )}
-              </Menu.Item>
-            </div>
-          </Menu.Items>
-        </Transition>
-      </Menu>
-      <div class="flex flex-col items-center pb-10">
-        <img class="w-full h-56 mb-3" src={fridgeImg} alt="Fridge image" />
-        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">
-          {name}
-        </h5>
-        <span class="text-sm text-gray-500 dark:text-gray-400">{id}</span>
-        <div class="flex mt-4 space-x-3 md:mt-6">
-          <a
-            href="#"
-            class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-[#65C9FF] rounded-lg hover:bg-[#65C9FF] focus:ring-4 focus:outline-none focus:ring-[#65C9FF] dark:bg-[#65C9FF] dark:hover:bg-[#65C9FF] dark:focus:ring-[#65C9FF]"
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-100"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95"
           >
-            Open
-          </a>
-        </div>
+            {/* Dropdown menu */}
+            <Menu.Items className="absolute right-0 mt-2.5 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow w-44">
+              <div className="py-1">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-black",
+                        "px-4 py-2 w-full text-sm flex justify-start items-center"
+                      )}
+                    >
+                      Edit
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-black",
+                        "px-4 py-2 w-full text-sm flex justify-start items-center"
+                      )}
+                    >
+                      Export Data
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      type="submit"
+                      className={classNames(
+                        active ? "bg-gray-100 text-gray-900" : "text-black",
+                        "px-4 py-2 w-full text-sm flex justify-start items-center"
+                      )}
+                    >
+                      Delete
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
+          </Transition>
+        </Menu.Button>
+      </Menu>
+      <div class="flex flex-col items-left pb-10">
+        <h5 class="mb-1 text-2xl z-10 w-2/3 font-black text-sky-950">{name}</h5>
+        <span class="text-md text-gray-500">{id}</span>
+        <img
+          class="absolute top-12 left-16 z-0 w-full h-52 mb-3"
+          src={fridgeImg}
+          alt="Fridge image"
+        />
       </div>
     </div>
   );
--- src/pages/Fridges.js
+++ src/pages/Fridges.js
@@ -2,19 +2,26 @@ import NavBar from "../components/UI/NavBar";
 import FridgeCard from "../components/UI/Fridge-card";
 
 const Fridges = () => {
+  const colors = ["bg-sky-200", "bg-sky-100", "bg-blue-200", "bg-indigo-200"];
   const fridges = [
-    { id: 1, name: "fridge1" },
-    { id: 2, name: "fridge2" },
-    { id: 3, name: "fridge3" },
+    { id: 1, name: "Frigo familiale" },
+    { id: 2, name: "Frigo chez mamie !!!!!!!!" },
+    { id: 3, name: "Appart Villejuif" },
   ];
 
   return (
-    <div class="bg-[#F9FBFC] h-screen">
+    <div>
       <NavBar />
-      <div class="flex justify-center ">
-        {fridges.map((fridge) => (
-          <FridgeCard id={fridge.id} name={fridge.name} />
-        ))}
+      <div class="flex h-screen justify-center items-center bg-[#F9FBFC]">
+        <div class="flex justify-center">
+          {fridges.map((fridge, i) => (
+            <FridgeCard
+              id={fridge.id}
+              name={fridge.name}
+              color={colors[i % colors.length]}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
