1d1562cc2bc09373c08b3f2cd201a2ac512d5bfb
MohamadReslan <115694912+MohamadReslan@users.noreply.github.com>
2023-05-16 20:25:25 +0200

    first commit

--- src/pages/Fridges.js
+++ src/pages/Fridges.js
@@ -1,8 +1,24 @@
+import Recipe from "../assets/images/svg/recipe.svg";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarOutlinedIcon from "@mui/icons-material/StarOutlined";
+
 const Fridges = () => {
-    return <div>
+  return (
+    <div>
       Fridges Page
-    </div>;
-  };  
-  
-  export default Fridges;
-  
+      <div className="w-40 h-24 text-[6px] p-4">
+        <div className="relative">
+          <div className="relative">
+            <img className="w-full" src={Recipe}></img>
+          </div>
+          <div className="absolute right-0 bottom-0">
+            <StarOutlinedIcon style={{ color: "white", width: "10px" }} />
+          </div>
+        </div>
+        <div>Sushi saumon, california and maki</div>
+      </div>
+    </div>
+  );
+};
+
+export default Fridges;
