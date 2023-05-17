f6c1103afa0fe3c31af87eb550c4b716656c1d97
MohamadReslan <115694912+MohamadReslan@users.noreply.github.com>
2023-05-17 17:43:23 +0200

    creation of the component

--- src/pages/Fridges.js
+++ src/pages/Fridges.js
@@ -1,40 +1,11 @@
-import Recipe from "../assets/images/svg/recipe.svg";
-import StarOutlinedIcon from "@mui/icons-material/StarOutlined";
-import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import { useState } from "react";
+import RecipeComponent from "../components/UI/RecipeComponent";
 
 const Fridges = () => {
-  const [starColor, setStarColor] = useState("white");
-  const initialColor = "white";
-
   return (
     <div>
       Fridges Pag
-      <div className="w-60 text-[6px] p-4 ">
-        <div className="relative">
-          <div className="relative">
-            <img className="w-full" src={Recipe}></img>
-          </div>
-          <div className="absolute bottom-0.5 right-1">
-            <StarOutlinedIcon
-              onClick={() => {
-                initialColor === starColor
-                  ? setStarColor("#F9C22F")
-                  : setStarColor("white");
-              }}
-              style={{ color: starColor, width: "12px", height: "16px" }}
-            />
-          </div>
-        </div>
-        <div className="mt-2 font-semibold">
-          Sushi saumon, california and maki
-        </div>
-        <div className="flex items-center text-[#666666]">
-          <AccessTimeIcon
-            style={{ color: "#666666", width: "12px", height: "16px" }}
-          />
-          <div className="ml-1">15 min</div>
-        </div>
+      <div>
+        <RecipeComponent />
       </div>
     </div>
   );
